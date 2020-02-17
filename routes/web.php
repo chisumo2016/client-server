0bcc49047fd46e96a6afb9f2b774d68e479c39c3
Bernard Chisumo <bchisumo74@gmail.com>
2020-02-17 12:22:07 +0000

    Client Server with Guzzle

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,31 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/redirect' ,function (){
+    $query = http_build_query([
+   'client_id'          => '4',
+    'redirect_uri'      => 'http://client-server.application/callback',  // client server
+     'response_type'   =>'code',
+        'scope'          =>''
+    ]);
+
+    return redirect('http://polling-voting-api.application/oauth/authorize?' .$query);  //application server polling community
+});
+
+Route::get('/callback', function (Illuminate\Http\Request $request){
+    $http = new \GuzzleHttp\Client;
+
+    $response  = $http->post('http://polling-voting-api.application/oauth/token',[  //application server polling community
+        'form_params' =>[
+            'client_id'          => '4',
+            'client_secret'  =>'NhAgouzeN0LMDWHjfR0VIBUnp4yKSpQHrdxJkd6b',  //application poll
+            'grant_type'  =>'authorization_code',
+            'redirect_uri' => 'http://client-server.application/callback' ,  // client server
+            'code' => $request->code
+        ],
+    ]);
+
+    return json_decode((string)  $response->getBody(), true);
+
+});
